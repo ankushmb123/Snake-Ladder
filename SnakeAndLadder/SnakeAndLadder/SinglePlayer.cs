a27977dbc964ab0c37711e33175040c1ae058ab4
Ankush Bahekar <ankushmb9@gmail.com>
2022-03-04 16:06:31 +0530

    check option fot no play,snake,ladder

--- SnakeAndLadder/SnakeAndLadder/SinglePlayer.cs
+++ SnakeAndLadder/SnakeAndLadder/SinglePlayer.cs
@@ -6,14 +6,36 @@ using System.Threading.Tasks;
 
 namespace SnakeAndLadder
 {
-    class SinglePlayer
+    internal class SinglePlayer
     {
+        public const int NO_PLAY = 0, SNAKE = 1, LADDER = 2;
         public int StartPoint = 0;
+        public int DiceRoll;
+        int Option = 0;
         public void StartPlaying()
         {
-            Console.WriteLine("Game start now");
-
-            Console.WriteLine("Starting point is " + StartPoint); //Main Start Point
+            Random Random = new Random();
+            DiceRoll = Random.Next(1, 7);
+            Console.WriteLine("player Roll  Dice number is:" + " " + DiceRoll);
+            Option = Random.Next(0, 3);
+            Console.WriteLine("Dice value:" + Option);
+            switch (Option)
+            {
+                case NO_PLAY:
+                    Console.WriteLine("No Play");
+                    break;
+                case SNAKE:
+                    StartPoint += DiceRoll;
+                    Console.WriteLine("Got Ladder:" + StartPoint);
+                    break;
+                case LADDER:
+                    StartPoint -= DiceRoll;
+                    Console.WriteLine("Snake Attack:" + StartPoint);
+                    break;
+                default:
+                    Console.WriteLine("Invalid Option");
+                    break;
+            }
         }
     }
 }
